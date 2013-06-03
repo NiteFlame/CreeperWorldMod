0cc47e769012d4d494e69a8276c784aa48dda451
NiteFlame <LiliyLDoe@aim.com>
2013-06-03 17:12:28 -0400

    do it

---
+++ Code/CreeperWorld/lib/Reference.java
@@ -0,0 +1,7 @@
+package CreeperWorld.lib;
+
+public class Reference {
+    public static final String MOD_ID = "CWM";
+    public static final String NAME = "Creep World Mod";
+    public static final String VERSION = "0.0.1";
+}
---
+++ Code/CreeperWorldMod/CreeperWorldModClass.java
@@ -0,0 +1,27 @@
+package CreeperWorldMod;
+
+import CreeperWorld.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+
+public class CreeperWorldModClass {
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    @Init
+    public void init(FMLInitializationEvent event){
+        
+    }
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event){
+        
+    }
+}
